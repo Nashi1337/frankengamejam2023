df1f7b80924a8f75ec8a2f68fa250789f24e2b46
Linda Rogner <linda.rogner@stud-mail.uni-wuerzburg.de>
2023-11-05 15:34:14 +0100

    added sound files and started working on audio manager replaced speech bubbles

---
+++ FGJ2023/Assets/AudioManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource meow;
+    [SerializeField] AudioSource meow2;
+    [SerializeField] AudioSource exerted;
+    [SerializeField] AudioSource grunt;
+    [SerializeField] AudioSource grunt2;
+    [SerializeField] AudioSource grunt3;
+    [SerializeField] AudioSource grunt4;
+    [SerializeField] AudioSource grunt5;
+    [SerializeField] AudioSource grunt6;
+    [SerializeField] AudioSource dinoChewing;
+}
